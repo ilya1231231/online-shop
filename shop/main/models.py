5880765c9c57b55f4594644c627b1e4615f198e3
ilya1231231 <malygin.ilja@yandex.ru>
2021-07-19 21:24:50 +0300

    Created some models
    Created "Hookah" and "Tobacco" models

--- shop/main/models.py
+++ shop/main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 from django.contrib.auth import get_user_model    #Хотим использовать пользователя обозначенного в настройках django
 User = get_user_model()    #Хотим использовать пользователя обозначенного в настройках django
@@ -26,6 +27,9 @@ class Category(models.Model):
 
 class Product(models.Model):
 
+    class Meta:
+        abstract = True
+
     category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)     #показывает,что товар пренадлежит категории
     title = models.CharField(max_length=255, verbose_name='Наименование товара')
     slug = models.SlugField(unique=True)
@@ -40,8 +44,11 @@ class Product(models.Model):
 class CartProduct(models.Model):
 
     user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)     #Внешний ключ на пользователя(кастомера)
-    cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE)       #Внешний ключ на корзину
-    product = models.ForeignKey(Product, verbose_name='Продукт', on_delete=models.CASCADE)     #Внешний ключ на продукт
+    cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products')     #Внешний ключ на корзину
+    content_type= models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = (GenericForeignKey('content_type', 'object_id'))
+    #product = models.ForeignKey(Product, verbose_name='Продукт', on_delete=models.CASCADE)     #Внешний ключ на продукт
     count = models.PositiveIntegerField(verbose_name='Количество', default=1)    #количество товара
     total_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
 
@@ -53,12 +60,12 @@ class CartProduct(models.Model):
 class Cart(models.Model):
 
     owner =models.ForeignKey('Customer', verbose_name='Владелец', on_delete=models.CASCADE)    #Владелец корзины
-    products = models.ManyToManyField(CartProduct, blank=True)     #!!!
+    products = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart')     #!!!
     total_products = models.PositiveIntegerField(default=0)
     total_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
 
     def __str__(self):
-        return str(self.id)
+        return str(self.id)     #!!!
 
 
 class Customer(models.Model):
@@ -68,15 +75,26 @@ class Customer(models.Model):
     address = models.CharField(max_length=250, verbose_name='Адрес пользователя')
 
     def __str__(self):
-        return 'Покупатель: {} {}'.format(self.user.first_name, self.user.last_name)
+        return 'Покупатель: {} {}'.format(self.user.first_name, self.user.last_name)     #!!!
 
 
-class Specifications(models.Model):
 
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    name = models.CharField(max_length=255, verbose_name='Имя товара для характеристик')
-
+class Tobacco(Product):
+    taste = models.CharField(max_length=255, verbose_name='Вкус табака')
+    strength = models.CharField(max_length=255, verbose_name='Крепость табака')
+    structure = models.CharField(max_length=255, verbose_name='Нарезка табака')
+    size = models.CharField(max_length=255, verbose_name='Возможные граммовки')
 
     def __str__(self):
-        return 'Характеристики для товаров: {}'.format(self.name)
+        return '{} : {}'.format(self.category.name, self.title)
+
+class Hookah(Product):
+    high = models.CharField(max_length=255, verbose_name='Длина шахты')
+    in_material = models.CharField(max_length=255, verbose_name='Материал шахты')
+    out_material = models.CharField(max_length=255, verbose_name='Материал накладки')
+    pull = models.CharField(max_length=255, verbose_name='Тяга')
+
+    def __str__(self):
+        return '{} : {}'.format(self.category.name, self.title)
+
+
