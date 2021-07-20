1697031f9b94547fc01b1470435a30326651f245
ilya1231231 <malygin.ilja@yandex.ru>
2021-07-20 08:34:08 +0300

    Created right category choice field for  admin site

--- shop/main/admin.py
+++ shop/main/admin.py
@@ -1,10 +1,43 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
+from django import forms
+
+
+
+
+'''Функция выбора категории,характерной для продукта <Tobacco>'''
+class TobaccoCategoryChoiceField(forms.ModelChoiceField):
+
+    pass
+
+
+class TobaccoAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return TobaccoCategoryChoiceField(Category.objects.filter(slug='tobaccos'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+
+'''Функция выбора категории,характерной для продукта <Hookah>'''
+
+class HookahCategoryChoiceField(forms.ModelChoiceField):
+
+    pass
+
+
+class HookahAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return TobaccoCategoryChoiceField(Category.objects.filter(slug='hookahs'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 admin.site.register(Category)
-admin.site.register(Tobacco)
-admin.site.register(Hookah)
+admin.site.register(Tobacco, TobaccoAdmin)
+admin.site.register(Hookah, HookahAdmin)
 admin.site.register(CartProduct)
 admin.site.register(Cart)
 admin.site.register(Customer)
