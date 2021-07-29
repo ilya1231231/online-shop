a39c7e681f245dc46602d23cf998deca78e1b4f2
ilya1231231 <malygin.ilja@yandex.ru>
2021-07-29 11:32:36 +0300

    created cart

--- shop/main/urls.py
+++ shop/main/urls.py
@@ -1,14 +1,13 @@
 
-from .views import BaseView , ProductDetailView, CategoryDetailView
+from .views import BaseView, ProductDetailView, CategoryDetailView, CartView
 from django.urls import path
-
-
 
 
 urlpatterns = [
     path('', BaseView.as_view(), name='test'),
     path('products/<str:ct_model>/<str:slug>/', ProductDetailView.as_view(), name='product_detail'),
     path('category/<str:slug>/', CategoryDetailView.as_view(), name='category_detail'),
+    path('cart/', CartView.as_view(), name='cart')
 ]
 
 '''ct_model и slug из функции get_absolute_url, name -это viewname, которое мы передавали в функцию GAU моделей
--- shop/main/views.py
+++ shop/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, View
-from .models import Tobacco, Hookah, Category, LatestProduct
+from .models import Tobacco, Hookah, Category, LatestProduct, Customer, Cart
 from .mixins import CategoryDetailMixin     #импорт миксина!!!
 
 
@@ -44,4 +44,19 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
     context_object_name = 'category'
     template_name = 'main/category_detail.html'
     slug_url_kwarg = 'slug'
+
+
+class CartView(View):
+
+    def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer)
+        categories = Category.objects.get_categories_for_up_sidebar()
+        context = {
+            'cart': cart,
+            'categories': categories
+        }
+        return render(request, 'main/cart.html', context)
+
+
 
